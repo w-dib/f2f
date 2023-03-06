d2092d425f5093ed604267a603b5880f4767c86c
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-03-06 23:23:41 +0400

    Fix Nextauth login issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,27 +4,16 @@ import LinkedInProvider from "next-auth/providers/linkedin";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export const authOptions = {
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
-  },
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
     LinkedInProvider({
       clientId: process.env.LINKEDIN_CLIENT_ID,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     }),
   ],
+  adapter: FirestoreAdapter(),
 };
-
-export default NextAuth(authOptions, { adapter: FirestoreAdapter() });
+export default NextAuth(authOptions);
